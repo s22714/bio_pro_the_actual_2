7421ddec2564e27b4734a7df9ca1db700b311890
s22714 <s22714@pjwstk.edu.pl>
2024-06-03 19:11:48 +0200

    dg

--- script_8.py
+++ script_8.py
@@ -32,17 +32,6 @@ alignments = aligner.align(seq_1, seq_2)
 n = 3
 alignment = alignments[n]
 
-coord_matrix = np.zeros((2, len(alignment.coordinates[0])))
-
-for i, (start, end) in enumerate(zip(*alignment.coordinates)):
-    coord_matrix[0, i] = start
-    coord_matrix[1, i] = end
-
-with open('alignment_info.txt', 'w') as f:
-    f.write(f"AlignmentCounts (gaps={alignment.counts().gaps}, identities={alignment.counts().identities}, mismatches={alignment.counts().mismatches})\n")
-    f.write(str(coord_matrix))
-    f.write(f"\n\n{str(alignment)}")
-
 print(f"AlignmentCounts (gaps={alignment.counts().gaps}, identities={alignment.counts().identities}, mismatches={alignment.counts().mismatches})")
-print(coord_matrix)
+print(alignment.coordinates)
 print(alignment)
